7b3db8274c73389f200321de44130473fe762105
User <User@DESKTOP-ESP2JT1>
2022-10-28 12:05:28 +0300

    Add project files.

---
+++ Circle Area and Perimeter/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// read
+double r = Double.Parse(Console.ReadLine());
+// calculate
+Double area = Math.PI * r * r;
+Double perimeter = 2 * Math.PI * r;
+
+// print
+Console.WriteLine($"{perimeter:f2}");
+Console.WriteLine($"{area:f2}");
+
---
+++ triengle/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+{
+    // read input 
+    // a
+    // h
+
+    double side = double.Parse(Console.ReadLine());
+    double hieght = double.Parse(Console.ReadLine());
+
+    // calculate
+    // a * h / 2
+    double area = side * hieght / 2;
+    // print output
+    // f2
+    Console.WriteLine($"{area:f2}");
+}
